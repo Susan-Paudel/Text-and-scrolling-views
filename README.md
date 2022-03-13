1143b19fa845982eea0b470bd5d376869c5ab364
Susan-Paudel <psusan19@tbc.edu.np>
2022-03-13 14:53:48 +0545

    readme file update

---
+++ README.md
@@ -0,0 +1,5 @@
+# Scrollview ss
+![alt text](ScrollView.png)
+
+#record
+![alt text](Record.gif)
--- app/src/main/java/com/example/scrollingtext/MainActivity.java
+++ app/src/main/java/com/example/scrollingtext/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.scrollingtext;
-
+//import required library
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+//MinActivity child class inherit all the property of AppCompatActivity i.e. parent
 public class MainActivity extends AppCompatActivity {
-
+    //Activities have the ability,to restore themselves to a previous state using the data stored in this bundle
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //super keyword is used to refer parent class object
         super.onCreate(savedInstanceState);
+        //it set the XML file as your main layout when the app starts
         setContentView(R.layout.activity_main);
     }
 }
